850e350d86444126a3cb778bc6330f568bcc9118
vixplows <vicky@vickyplows.co.uk>
2017-07-03 17:55:30 +0100

    Pass getRankingListTest

--- app/src/main/java/com/example/vicky/movieranking/RankingList.java
+++ app/src/main/java/com/example/vicky/movieranking/RankingList.java
@@ -1,8 +1,30 @@
 package com.example.vicky.movieranking;
 
+import java.util.ArrayList;
+
 /**
  * Created by vicky on 03/07/2017.
  */
 
 public class RankingList {
+    private ArrayList<String> rankingList;
+
+    public RankingList() {
+        this.rankingList = new ArrayList<String>();
+    }
+
+    public ArrayList<String> getRankingList() {
+        return new ArrayList<String>(rankingList);
+    }
+
+//    public int getLength() {
+//        return rankingList.size();
+//    }
+
+//    public void setUpRankingList() {
+//        String [] rankings = {
+//
+//        }
+//    }
+
 }
--- app/src/test/java/com/example/vicky/movieranking/RankingListTest.java
+++ app/src/test/java/com/example/vicky/movieranking/RankingListTest.java
@@ -1,8 +1,31 @@
 package com.example.vicky.movieranking;
 
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
 /**
  * Created by vicky on 03/07/2017.
  */
 
 public class RankingListTest {
+
+    RankingList rankingList;
+
+    @Before
+    public void before() {
+        rankingList = new RankingList();
+    }
+
+    @Test
+    public void getRankingListTest() {
+        assertNotNull(rankingList.getRankingList());
+    }
+
+//    @Test
+//    public void setUpRankingList() {
+//        assertEquals(10, rankingList.getLength());
+//    }
 }
